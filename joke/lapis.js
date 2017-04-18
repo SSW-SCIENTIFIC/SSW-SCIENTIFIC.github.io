37e36f11cacf2adad4ff8703bd7669b71a19a5d6
S-sword <S-sword@s-sword.net>
2017-04-19 00:25:46 +0900

    おりゅ？

---
+++ joke/lapis.js
@@ -0,0 +1,196 @@
+// customized from SL.JS <>
+
+(function () {
+    let AAArray = [];
+    AAArray.push('                                                                                                                                                                         ...+gJ..');
+    AAArray.push('                                                                                    ...............                                                                    .d"!    .TN,');
+    AAArray.push('                                                                            ..JWHB9UOI=lllllllllOZVTHMHa...                                                          .d"          (N,');
+    AAArray.push('                                                                       ..X"^ .<<_--__~~.~~.~~~<?<1lzlllllOTHNgMHSa...((..                                           .M\'             Tb');
+    AAArray.push('                                                                    .J"!  ._-?<<<<<<?771-_~.~~-Jv>~_<?zlllllllOTMQxz??-.Tm.                                        .M!               ?h.');
+    AAArray.push('                                                                 .J"   .~~_~._ ~~~~~~~~~~~~~~_v>~(((--~_?1llllllllZHNe?<.?MY6u(,.                                  d\                 /b');
+    AAArray.push('                                                               .V^  .~~~.~_  .~.~..~..~..~..~.~~_?7<??<<~~~1l==l==lllvHmx+Jo(.. ?TQ,                              .F      .........    ?b');
+    AAArray.push('                                                            ..d!  -~~.~.~  .~~~~.~~.~~.~~.~~.~..~~~~.~~~~~~~_1ll=ll=llllTNkWUWHme-..T&.                           M!     .?????????     W|');
+    AAArray.push('                                                 .. ..JJ+.V6d6<?5J~.~~._  .~.~.~~.~~~~~.~~.~~~~.~.~.~~..~..~~~(1ll=l=l=lll?NkZuC?Tmzz-7+                         .F      ((((((((((,    .N.');
+    AAArray.push('                                           .-VY""71ztvTUM1?1????<,N/~~_  ~.~~.~.~.~. .~.~~.~..~~.~.~~.~~.~~..~._1ll=l=l=l=llTmUkZo(WsllzT,                       d\               M>     Jb');
+    AAArray.push('                                       ..#"~(Jd""7!~~(?TWMQagMHYWBdN._ .~.~.~~.~  ~  ~~.~.~~.~~.~~_~.~~.~~~~~.~.~?ll=lll=ll=lvkzWZkJWslz-?h,                    .#              .d$       N.');
+    AAArray.push('                                     .#3~-(#=.(zlllllllllllvMNXZZkz?h..~(<~.~~.  ~  ~_ _.~~.~~.~~_ ~.~.~.~.-_~~~.~?ll=l=ll=lllzMe4ZZwMAyllide                   .F        ....J#"         J]');
+    AAArray.push('                                  .-8<~~(M3zlv! ....(zlllllllWMkuZS<.N_(=<~~~.  ~_ ._  .~~.~.~.~._ ~~.~~.~..  _~~.~(=lll=ll=l=lzM2WZuXNpky-??h.                 J%               ..       .N.');
+    AAArray.push('                                 (5~~~-JN0lllllz((zllll=lllllzNWXZX2_(plv~~_.  ~_  _   ~~.~~~.~ ~_ ~~~.~~.~~_  _~_~.(ll<?zzzllllvNzHZZdNppkz.(N.                M         g.    ,<.%       d[');
+    AAArray.push('                               .B<~~~-dpkllll<....+llllll=lllldkZuZS< Nl>(<~..(~  -     _ ___~  ~  ~  .~~~~~._  ~_-~~<ll<_1zzl=lldRdkZZMHppklzJM.              .#         M~   ...g.       ,b');
+    AAArray.push('                             .d=~~.~-dppUUV?!  ??1lll=lllll=lld#ZZuXz_dOiC~~-z~_        - . .~  ~  ~  ~..~_  ~. -~_-~~1Oz_(_jll=lzMcXZXuMkpppWAzN.             .F         MKT"""!           N');
+    AAArray.push('                            .#~~-~~_JHpkll1(((((((+llll=llllllWNZZuXZ:JkP~~(z:~         _ ~_~_  ~  .  . _~.   _  ~~(_~(Zl1~_~Ol=lldbzHX?XMkppZllvN             d\         M~                d|');
+    AAArray.push('                           .Y~~(>~.(NppUUVOv<< (lllllllllllllzMHZuZXR<JN:.(zv~_   .  ~~~~.~.(<  ~   _      .  __ _~.1~~O=l<_<(Zl=lzNzdkX-CWHpfO?<db            M          M_                J]');
+    AAArray.push('                          .$~~~(<.(NppWllllz1<+((zll=lllllllzWM@ZuZXD?j#~-zl:._  .~ ..~.~~_(d~_ ~_  ~  .   _   _  .~(<.jllz~((Xll=ldR?dkZo?XMkZ1..d;          .#          ?BHpppppb]        ,#');
+    AAArray.push('                         .P~.~_=~~dHppRysyi....  ?1lllllllwXAdm#ZZuXR??N~+lv~~_ .~.-~~.~.-+d0~._~_  ._ _._~~.  ~  ~~(r~(ll=:(ldZllld#???WZZAXZWAOlzN          .F                             N');
+    AAArray.push('                         J>.~.(=_(#ppp0VOC<<<11llllllllOUWAzdMH#ZuZX$??d+ll>.~_ ~~~.~~(~(zd$z~~~.~. -.  .~~~...~_ ~._O~(Zllz(llSl=lzN?uz?vXuZXWIz.(d]         (]         .((((((((.          M-');
+    AAArray.push('                        .@~~~_zz~(NpppIlz+zzz+(.._?zlllzOXT8HgMSZuZX$??dRlz_~._-~.~~.(:_zuD;z~.~~~~  ~~.~.~.~~~.~ .~~d_~jlllz=ld=l=lHzd??1gkHfpkwv<zN         d\                (F           d[');
+    AAArray.push('                        J{~.~(lz~dHppWlll~     _?1lllVWkzdpx.<7HkZuX$?<zNl>~~(>~<~.~~J~+lZ;<J_~.~_.~.~.~.~.~~.~~...~~d;~(kll=lldZlzO#1J7HMgMkffSlllld;        M!              .d$            J]');
+    AAArray.push('                        #~.~.(lz(MpppWXkzwOI..(zzuzzllluWpWM+..:?NkXR?>(dy>.~(:_z~~.(l(ld>!.+[~~(:~.~.~.~~~.~~.~~_.~.dI.~?y=l=lzI=lZ<(Mh_?gmMkpRIz.?db        N             ..#N,            ,F');
+    AAArray.push('                       .F~~~~+=z(NpppWll?! . _~?1lOUUAdpppMmNx!..:TmS?<.?N<~_z<(l/~~J(lj$<  <O~.(I_~~.~~.~._-~~.~._--dS-.~OylllzIld<y(NHr~dmmMfkzl==zN       .#           .gD   (HJ          ,#');
+    AAArray.push('                       ,$~..~zl=JNpppWllllllllllllllldpppWNmH2:~ ~:dNz?<.1$_(l<+lG.~r(lk<    ?;_j0>~~~.~~.~~<-~(+<?<<dRI?71WyllzOd$JX+MgD<JHMHf$=i.?=H.      .#         d"\'       (B!        .#');
+    AAArray.push('                       .r~~._lllJNpppflllzzzlllllOUWfppppMMmgb<_~:::?NgggHB=+l<lld~($ldI+.+v774<OWk+.~~.i_~~(z__~~~~+dWz1~~~XRzzlK(JZcMHI_dyVffUw>?-(d~      ,F                              .N');
+    AAArray.push('                       Mr~.~_l=ldNpfppkX0:  ..JlllldppffpMmmmbx<:::::dKlllz(llzlld_(mU#^       l(ddz-~.~_1~._Z>~.~~(ld+Sl+.~~/Wzd$JZZXdI:(HyVVkzz<. 1d~      (F             M   ,N,           M');
+    AAArray.push('                      .#]~~._l=ldNppppRlllll.JllllldpfpppMmmH@;++j&-:<Msl=l+l=lzuf=(Kd\        .czkWz-~~~(<~~wl_~.~(lZ;?Nzz-~~~?TagkUOv<(HyyyWC??!?<<W_      J]         ."""M"""! (M.         M.');
+    AAArray.push('                      ,#b~~.~lllvMpffpfZ==zu&yl=zZUWppfppMNNM#X0jZZX+:J#l=l=luZ6d%~JDd          ,+vcWy<~._1~.jl>~._luRz<>Ts=z+,_~~_<~~(J7NkyykOv<<- <MG      J]            .MgXXa-,           M_');
+    AAArray.push('                      ,#N_~~~1=llMHfpfpWuZ0C!  (1==dffpfpHMOdWWNZZZZX<(MZlluVIlu$~(ZRf      ~<-. ,xw.Ws1~~(_~Jlz~(J6dgMMMMMMNNslOz+jgXI: .?4I+<_. (_(#(|     J]         .J" M     U]          M_');
+    AAArray.push('                      ,#d[.~.(llldNfffffkz????????1dVfffVVMR#yKdkZZG+2:M0lOIlldt_(Z>jD        (z. .1G.Vm1_(>~Jlz(zgMMMMN-~dMMBMW#zUZZZX+(<. .: :(_.<dt~G     (F         JL  M     (F          M{');
+    AAArray.push('                      ,#vN~.~(ll=lMHfffVVkxuwVC<>>>?dVVVyVVMXyHzMkZZZI:dRllluZ!(jf< .]        .$~   ?G.?Kz_z~J==(MMNHMgM\ (#<j8dM(ZZuuZZc:::<::::;:j#~~J;    ,F          (""^ ?"""!           M{');
+    AAArray.push('                      ,N(dc~~_1l=ldNffVyyyyk;;;;+++>?UyyyyWHyZURldNkI+:dKluV:~(d6!   <  ...._!        < /pz?~dldMMMMH#-,  ?>;KlzM2OXZZuZZAyV<:::::<dt~.(b    ,#           .,                  M{');
+    AAArray.push('                      .#<zN_~.(lllldNVyyyyyyk&jXV>;<<jXyWWyyZZZUNylZBk+d3<~_(J3<!       ...JgNMMMK=!     ?Zl_kzXM#>!.j@ .<<;j6lldNJXZZZZuZXo(+>:+zd8>~._H     N           J].g""Ya            M:');
+    AAArray.push('                       N_(db~~_1lOllMNyyyyyZZyyk&d0<;;jdZyyZZyZZZUMmgaaggkVW+++((MNNNMMMMMMMMD!?7TNJ.     1l(0u67"UZX0><;<.(dllu@dMpjwZZZZZZZk&+Xd8l:~~~W     q           dq$    Jb           M ');
+    AAArray.push('                       d{~(dr~.(=dzllMNyyZyZZZZyZZyyZyyZyZZZyZZZZZZZZ>+#lllvr7MMMM@4M@MMMMD (             .zJO=_....~~_<;;;>Slld6lvMNvXZZZZZZ0UgMIlz~.~.d_    ,.          dF     ,#           M');
+    AAArray.push('                       ,b~~1d/~~(zWllldNWyyyyZZyZZZZyZZyZZyZZZZZZXwO><d6l=llL   (Wp WR<__(X"..             <Z!. .....~~._>;j3lu#lllzWU0gawAggH8IlSl>~~.~d     .~          ?3     .F          .N');
+    AAArray.push('                        N-~_1w/~~_1HzllvHRyXyyZyyyyZyyZyyZyyZZZZZZZ0<j@lZl=ld.      .(6wkXT^               ~     ........_;Z~1dIl==ldRlllllllll=dIv~~.~_H     .:              ..dD           .#');
+    AAArray.push('                        -b~~~?l-~~_1WsllldNswyyyZyyyZyyZyyZyZyyVTTT>j@lldzl<<S       ...........                    .  .  (:_dSll=lldEll=====lllXl:_~~.(#    -F             ?"^              .#');
+    AAArray.push('                         W;.~~_1_.~~_jHslll7MgZUXyyyyyWyyyyUyyyykc<j@ll=lXy1~(h  ..  .~~.~~....               R<         .3(d6l=llllWXll=lll=lldI> ~.~~(F  .M=                  .            .F');
+    AAArray.push('                         .N-~.~~_~.~~~_?4Azl=lTSxzUyyykZWyyWxvVC>jdMyWzllldm<~(h.......~.......                         ,>(#Il=l==ld#dlllll=lldS>  ~~(aduJ#=   ...         .] .JMS&,         J%');
+    AAArray.push('                          ,b_..~.~~..~~_~~?77COzudWWgAZO?OC11?+uW6ldyzWGzllzWg-_4,...........                         .1J#X6l=llllldDdll=lllldS>  ..~.(FT"BW""" M|         duf  M_ M.        M:');
+    AAArray.push('                           ?b~~~.~~.~._.         _~~.~?TYMMMHBUOlllzNlQ ?WslllTNgJH, .  .                            ?!_J(Dl=ll=llu#ldl=l=lld6>  .~..(@         (b         dF.L M .M        .N');
+    AAArray.push('                            ?b_~.~.~~~~.~.~~_...      _~.~~(T4ezll=lvNd[   7TOzMx ?""6-.                    .-j{      .0.Dllllllzld6z0===luXv!   .~.(#          .N         .  .M@"=         .#');
+    AAArray.push('                             ?N-~.~~.~.~~.~~.~~~.~.._..  _.~..~?WslllvHX.      .Ne.                    ..J&JVpf      .Y.klll=l=dld#=v===1d$<.__ ...(@            M;          ,^             J]');
+    AAArray.push('                              (No-~.~.~~.~.~~.~.~.~~_  _.-  _~...(4elllVh.      H+4,              ,W0XJrOv1?1>     .Z! dllv~~<lIldIz===z6<_...- .-JY             J]                         M!');
+    AAArray.push('                               .Wmz-~~.~~.~.~.~.~.~...-.   __ _.~..(WslllTo...J"Wz;>7n,             .777<<<!     .J!  ,Iv<~~~_zOd8=??zI=>...~_._(Y               .@         ...,           .#');
+    AAArray.push('                                 ?Hmz+-~~~.~~.~~..~.~..~..-   _......7mllllTb. .Jz;;;;<Tn,                     .?    .Dv~~..~~du#=?????>....._-=                  W,       .^  ?N.         Jt');
+    AAArray.push('                                   .TNslz--_~~.~.~..~........_.  .....?NllllzW, Ux>;;;;;;<75+,               .=      (v_ .~~.~jd1?????:...~(J^                    ,b           .@         .# ');
+    AAArray.push('                                      ?YNazll=lz+(--_~~.~.~....   .....?Kll>~~?h,vS&+<>>;;;;;<vYn-.        .7        K_     ~~jR?????:....(M                       M.        (@^          .F');
+    AAArray.push('                                           ?9XQgsz==???+(-_.....   .~...dkl>~.~~T,<==zvUG&+;;;;;;;;?TTQJ(JHF   ..... P~     ..(b????<..._+Mb.                      (b        (^          .M ');
+    AAArray.push('                                                 _?"6Qxz???+(-~..._ ....(Nlz_~.~.v,-1==?===7SJ;;;;;;;;d8zuW-ZC=??=>R.b~_ .__.~.4z??<_.._+dCCTe                      M,       dD          -F');
+    AAArray.push('                                                      .TMQx????+_._ _....MZl<~__~~4. _1=??=?=zTe>;;;jXuQW8VC=?=?=v(MmN~..~__....7ez:.._+?zI???N.J+,.                ,b                  .#');
+    AAArray.push('                                                        .hzTQx>??+-......dRl~._-~.(h....J77771az0+jJKQHUwC???===z!JmmMb-..._<_..._?n-.(?>??>??d#Xw&J3,               UL                .M\'');
+    AAArray.push('                                                         ,Z?dKNx??><..~..d0v.~_  ~.q           .7Wz==dkZ=zzz=?=<  D?NHHN+--_._<-....?e<?>>?>>?zMHHHkuxC,              W,              .d^');
+    AAArray.push('                                                         .3?1b?dk>??<..._MI:.. _  .J_            .b1=zNQXVC==<!   j+<TkZWmz??+-(+-...(mx&+&+?>dNqqHMHkX+1.             H,            .M^');
+    AAArray.push('                                                        .#??1b>??k???<..(#v.~_    ~(L         ..J6P _jD==?<!       S  /RZXN7&?????<~..(#Y1??zjMHqkqqqMNkGJ,             We          .#\'');
+    AAArray.push('                                                     .JW#???d3?>>dz>?<.(MI:.. .   .Jd,        ~!  >-,?73(..  ......,}  /KZXb ?e????<_..J2>>?jMWHqqkqkqqMkuzl             ?N,      .d"');
+    AAArray.push('                                                   .Y!.f1???f?>?>?I>?<(d$<.._..  .-P;d,         .!(X    .1Xk+  7(.      4Xud/ ,b>???:..(b>+gMHkXWqqkqqqqMHkwn              ?YNggWB=');
+    AAArray.push('                                                 .V   J3?>?v>?>>?????d#1<~..__....J!(j]       ./ (zX      .4Uk&.   ?7"=1JkXZb  (2>??>.._NgMMqqqHkWqqkqkqqHNkXn.');
+    AAArray.push('                                                .F    W??>?>>>?>>?udYz?<~.__...._J\'./~]     .J\' ,=zW        ?XUko. .      ."W_ ,P?>?{._7NqqqqqqqqkXHqkqkqqqMkuW.');
+
+    let AA = '';
+    AAArray.forEach(function (entity, index) {
+        AA += '<p style="margin:0;line-height:1em;font-size:6pt;">' + entity
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/ /g, '&nbsp;')
+            + '</p>';
+    });
+
+    //------------------------------------------------------------
+    // SL Initialize
+    //------------------------------------------------------------
+    let sl_speed = 100;
+    let sl_pitch = 15;
+    let sl_tx_color = "#FFFFFF";
+    let sl_bg_color = "#000000";
+    let sl_counter  = 0;
+    let sl_position = 0;
+    let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    let windowWidth = null;
+    if (window.opera || document.layers) {
+        windowWidth = window.innerWidth;
+    } else if (document.all) {
+        windowWidth = document.body.clientWidth;
+    } else if(document.getElementById){
+        windowWidth = window.innerWidth;
+    }
+    let sl_style_base
+        ='display: block;'
+        +'position: absolute;'
+        +'text-align: left;'
+        +'overflow: visible;'
+        +'white-space: pre;'
+        +'font: 12px/12px monospace;';
+
+    let sl_style_main
+        =sl_style_base
+        +'top: '+(scrollTop+100)+'px;'
+        +'left: '+windowWidth+'px;'
+        +'padding: 20px;'
+        +'z-index: 999;'
+        +'color: '+sl_tx_color+';';
+
+    document.body.innerHTML += '<div id="__sl_main__" style="'+sl_style_main+'">' + AA + '</div>';
+
+    var sl_w = document.getElementById("__sl_main__").clientWidth;
+    var sl_h = document.getElementById("__sl_main__").clientHeight;
+
+    var sl_style_background
+        =sl_style_base
+        +'top: '+(scrollTop+100)+'px;'
+        +'left: 0px;'
+        +'width: '+windowWidth+'px;'
+        +'height: '+sl_h+'px;'
+        +'z-index: 998;'
+        +'background-color: '+sl_bg_color+';'
+        +'filter: alpha(opacity=0);'
+        +'-moz-opacity: 0.0;'
+        +'opacity: 0.0;';
+
+    document.body.innerHTML += '<div id="__sl_background__" style="'+sl_style_background+'"><br /></div>';
+
+    //------------------------------------------------------------
+    // Actions
+    //------------------------------------------------------------
+    var sl_bg_counter = 0;
+
+    /**
+     * sl_open (gradually open background)
+     */
+    sl_open = function() {
+        var oid = "__sl_background__";
+        var op  = sl_bg_counter;
+        var ua  = navigator.userAgent
+        document.getElementById(oid).style.filter = 'alpha(opacity=' + (op * 10) + ')';
+        document.getElementById(oid).style.MozOpacity = op / 10;
+        document.getElementById(oid).style.opacity = op / 10;
+        if ( sl_bg_counter < 8 ) {
+            sl_bg_counter++;
+            setTimeout('sl_open()',100);
+        } else {
+            sl_run();
+        }
+
+    }
+
+    /**
+     * sl_run (move a train)
+     */
+    sl_run = function() {
+        document.getElementById("__sl_main__").innerHTML = AA;
+        document.getElementById("__sl_main__").style.left = windowWidth - sl_position + "px";
+        if ( sl_counter < 5 ) {
+            sl_counter++;
+        } else {
+            sl_counter = 0;
+        }
+        sl_position += sl_pitch;
+        if ( sl_w + (windowWidth - sl_position) < 0 ) {
+            sl_counter  = 0;
+            sl_position = 0;
+            document.body.removeChild(document.getElementById("__sl_main__"));
+            sl_close();
+        } else {
+            setTimeout('sl_run()',sl_speed);
+        }
+    }
+
+    /**
+     * sl_close (gradually close background)
+     */
+    sl_close = function() {
+        var oid = "__sl_background__";
+        var op  = sl_bg_counter;
+        var ua = navigator.userAgent
+        document.getElementById(oid).style.filter = 'alpha(opacity=' + (op * 10) + ')';
+        document.getElementById(oid).style.MozOpacity = op / 10;
+        document.getElementById(oid).style.opacity = op / 10;
+        if ( sl_bg_counter > 0 ) {
+            sl_bg_counter--;
+            setTimeout('sl_close()',100);
+        } else {
+            document.body.removeChild(document.getElementById(oid));
+        }
+    }
+
+    // start actions !
+    sl_open();
+})();
